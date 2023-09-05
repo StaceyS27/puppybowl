60af7b8124c909d21707443fd2912cba3ec3f663
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2023-09-05 16:15:10 -0400

    see details button

--- .history/script_20230905160655.js
+++ .history/script_20230905160655.js
@@ -16,16 +16,13 @@ const fetchAllPlayers = async () => {
   try {
     const response = await fetch(`${API_URL}/players`);
     const result = await response.json();
-    const playersArray = result.data.players;
+    const playersArray = result.data.players
     console.log(playersArray);
     return playersArray;
   } catch (err) {
-    console.error(
-      "Error: Your dog just chased a bug, resulting in a temporary glitch. We'll have it fetched in no time!",
-      err
-    );
+    console.error("Error: Your dog just chased a bug, resulting in a temporary glitch. We'll have it fetched in no time!", err);
   }
-};
+}
 
 //---------------------------------------FETCH SINGLE PLAYERS---------------------------------------
 /**
@@ -35,17 +32,15 @@ const fetchAllPlayers = async () => {
  */
 const fetchSinglePlayer = async (playerId) => {
   try {
-    const response = await fetch(`${API_URL}/players/${playerId}`);
+    const response = await fetch(`${API_URL}/players/${playerId}`)
     const result = await response.json();
     console.log(result);
     return result;
   } catch (err) {
-    console.error(
-      `Error:#${playerId} just chased a bug, resulting in a temporary glitch. We'll have it fetched in no time!`,
-      err
-    );
+    console.error(`Error:#${playerId} just chased a bug, resulting in a temporary glitch. We'll have it fetched in no time!`, err);
   }
 };
+
 
 //---------------------------------------ADD NEW PLAYERS---------------------------------------
 /**
@@ -56,11 +51,11 @@ const fetchSinglePlayer = async (playerId) => {
 const addNewPlayer = async (playerObj) => {
   try {
     const response = await fetch(`${API_URL}/players`, {
-      method: "POST",
+      method:"POST",
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(playerObj),
+      body: JSON.stringify(playerObj)
     });
 
     const player = await response.json();
@@ -68,10 +63,12 @@ const addNewPlayer = async (playerObj) => {
 
     const players = await fetchAllPlayers();
     renderAllPlayers(players);
+
   } catch (err) {
     console.error("Oops, something went wrong with adding that player!", err);
   }
 };
+
 
 //---------------------------------------REMOVE PLAYER---------------------------------------
 /**
@@ -80,14 +77,17 @@ const addNewPlayer = async (playerObj) => {
  */
 const removePlayer = async (playerId) => {
   try {
-    const response = await fetch(`${API_URL}/players/${playerId}`, {
-      method: "DELETE",
-    });
+    const response = await fetch(`${API_URL}/players/${playerId}`,
+      {
+        method: "DELETE",
+      },
+    );
     const result = await response.json();
     console.log(result);
 
     const players = await fetchAllPlayers();
     renderAllPlayers(players);
+
   } catch (err) {
     console.error(
       `Whoops, trouble removing player #${playerId} from the roster!`,
@@ -95,6 +95,7 @@ const removePlayer = async (playerId) => {
     );
   }
 };
+
 
 /**
  * Updates `<main>` to display a list of all players.
@@ -117,46 +118,45 @@ const removePlayer = async (playerId) => {
  */
 //---------------------------------------DETAILS BUTTON ON CARD---------------------------------------
 const getDetailButton = (player) => {
-  const detailButton = document.createElement("button");
+  const detailButton = document.createElement('button');
   detailButton.textContent = "See Details";
-  detailButton.addEventListener("click", (event) => {
-    renderSinglePlayer(player); //See details" button that, when clicked, calls `renderSinglePlayer`to display more information about the player
+  detailButton.addEventListener('click', (event) => {
+    renderSinglePlayer(player);//See details" button that, when clicked, calls `renderSinglePlayer`to display more information about the player
   });
   return detailButton;
-};
+}
 
 //---------------------------------------RENDER ALL PLAYERS---------------------------------------
 const renderAllPlayers = async (playerList) => {
   //Create error if there is no `playerList` or the `playerList`file is empty
   if (!playerList || playerList.length === 0) {
-    const h2 = document.createElement("h2");
-    h2.textContent =
-      "Error: Your dog just chased a bug, resulting in a temporary glitch. We'll have it fixed soon!";
+    const h2 = document.createElement('h2');
+    h2.textContent = "Error: Your dog just chased a bug, resulting in a temporary glitch. We'll have it fixed soon!";
     playerContainerEl.appendChild(h2);
     return;
   }
   //Clears the HTML for a clean file
-  playerContainerEl.innerHTML = "";
+  playerContainerEl.innerHTML = ""
   //
-  playerList.forEach((player) => {
+  playerList.forEach(player => {
     //Create container for the player card
-    const playerEl = document.createElement("div"); //Creating div element
-    playerEl.classList.add("playerCard"); //Creating class element for CSS
+    const playerEl = document.createElement('div');//Creating div element 
+    playerEl.classList.add('playerCard');//Creating class element for CSS
 
     //Render the Name
-    const playerNameHeading = document.createElement("h2");
+    const playerNameHeading = document.createElement('h2');
     playerNameHeading.textContent = `Name: ${player.name}`;
 
     //Render the ID
-    const playerIdHeading = document.createElement("h3");
+    const playerIdHeading = document.createElement('h3');
     playerIdHeading.textContent = `ID: ${player.id}`;
 
     //Render the Breed
-    const playerBreedHeading = document.createElement("h3");
+    const playerBreedHeading = document.createElement('h3');
     playerBreedHeading.textContent = `Breed: ${player.breed}`;
 
     //Render the Image
-    const playerImage = document.createElement("img");
+    const playerImage = document.createElement('img');
     playerImage.src = player.imageUrl;
     playerImage.alt = player.name;
 
@@ -169,55 +169,55 @@ const renderAllPlayers = async (playerList) => {
       playerBreedHeading,
       playerImage,
       getDetailButton(player),
-      getDeleteButton(player)
+      getDeleteButton(player),
     );
     //Append this player to main player container
-    playerContainerEl.appendChild(playerEl);
+    playerContainerEl.appendChild(playerEl)
   });
 };
 
 // Function to handle form submission and add a new player
 async function addPlayer(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const form = event.target;
-  const name = form.name.value;
-  const breed = form.breed.value;
-  const imageUrl = form.imageUrl.value;
+    const form = event.target;
+    const name = form.name.value;
+    const breed = form.breed.value;
+    const imageUrl = form.imageUrl.value;
 
-  const newPlayer = {
-    name: name,
-    breed: breed,
-    imageUrl: imageUrl,
-    status: "rookie", // You can set the default status as needed
-  };
+    const newPlayer = {
+        name: name,
+        breed: breed,
+        imageUrl: imageUrl,
+        status: "rookie" // You can set the default status as needed
+    };
 
-  try {
-    const response = await fetch(`${baseURL}/players`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newPlayer),
-    });
+    try {
+        const response = await fetch(`${baseURL}/players`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newPlayer)
+        });
+        
+        const result = await response.json();
 
-    const result = await response.json();
-
-    if (result.success) {
-      // Player added successfully, update the roster display
-      displayRoster();
-      form.reset();
-    } else {
-      console.error(result.error.message);
+        if (result.success) {
+            // Player added successfully, update the roster display
+            displayRoster();
+            form.reset();
+        } else {
+            console.error(result.error.message);
+        }
+    } catch (err) {
+        console.error(err);
     }
-  } catch (err) {
-    console.error(err);
-  }
 }
 
 // Add a submit event listener to the form
-const addPlayerForm = document.getElementById("add-player-form");
-addPlayerForm.addEventListener("submit", addPlayer);
+const addPlayerForm = document.getElementById('add-player-form');
+addPlayerForm.addEventListener('submit', addPlayer);
 
 //---------------------------------------RENDER SINGLE PLAYER---------------------------------------
 /**
@@ -237,48 +237,47 @@ const renderSinglePlayer = (player) => {
   //Create error if there is no `playerList` or the `playerList`file is empty
   playerContainerEl.innerHTML = " ";
   if (!player || player.length === 0) {
-    const h2 = document.createElement("h2");
-    h2.textContent =
-      "Error: Your dog just chased a bug, resulting in a temporary glitch. We'll have it fixed soon!";
+    const h2 = document.createElement('h2');
+    h2.textContent = "Error: Your dog just chased a bug, resulting in a temporary glitch. We'll have it fixed soon!";
     playerContainerEl.appendChild(h2);
     return;
   }
 
   //Clears the HTML for a clean file
-  playerContainerEl.innerHTML = "";
+  playerContainerEl.innerHTML = ""
 
   //Make a new player view container
-  const singlePlayerViewContainer = document.createElement("div");
-  singlePlayerViewContainer.classList.add("singlePlayerCardView"); //Class for CSS
+  const singlePlayerViewContainer = document.createElement('div');
+  singlePlayerViewContainer.classList.add('singlePlayerCardView');//Class for CSS
 
-  const playerInfoEl = document.createElement("div");
-  playerInfoEl.classList.add("player");
+  const playerInfoEl = document.createElement('div');
+  playerInfoEl.classList.add('player');
 
   //Render the Name
-  const playerNameHeading = document.createElement("h2");
+  const playerNameHeading = document.createElement('h2');
   playerNameHeading.textContent = `Name: ${player.name}`;
 
   //Render the ID
-  const playerIdHeading = document.createElement("h3");
+  const playerIdHeading = document.createElement('h3');
   playerIdHeading.textContent = `ID: ${player.id}`;
 
   //Render the Breed
-  const playerBreedHeading = document.createElement("h3");
+  const playerBreedHeading = document.createElement('h3');
   playerBreedHeading.textContent = `Breed: ${player.breed}`;
 
   //Render the Image
-  const playerImage = document.createElement("img");
+  const playerImage = document.createElement('img');
   playerImage.src = player.imageUrl;
   playerImage.alt = player.name;
-
+  
   playerInfoEl.append(
     playerNameHeading,
     playerIdHeading,
     playerBreedHeading,
     playerImage,
     getBackButton(),
-    getDeleteButton()
-  );
+    getDeleteButton(),
+  )
 
   //Append
   playerContainerEl.appendChild(playerInfoEl);
@@ -289,28 +288,28 @@ const renderSinglePlayer = (player) => {
 //Delete button for all players view or single player view
 
 const getDeleteButton = (player) => {
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("delete-button");
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete-button');
   deleteButton.textContent = "Remove";
 
-  deleteButton.addEventListener("click", (event) => {
+  deleteButton.addEventListener('click', (event) => {
     removePlayer(player.id);
-  });
+  })
 
   return deleteButton;
-};
+}
 
 //---------------------------------------BACK BUTTON---------------------------------------
 //Back button from single player view to main view
-const getBackButton = () => {
-  const backButton = document.createElement("button");
+const getBackButton = () =>{
+  const backButton = document.createElement('button');
   backButton.textContent = "Return Back";
-  backButton.addEventListener("click", async (event) => {
+  backButton.addEventListener('click', async (event) =>{
     const returnToPlayers = await fetchAllPlayers();
     renderAllPlayers(returnToPlayers);
   });
   return backButton;
-};
+}
 
 //---------------------------------------RENDER NEW PLAYER FORM---------------------------------------
 /**
@@ -320,70 +319,70 @@ const getBackButton = () => {
  */
 const renderNewPlayerForm = () => {
   try {
-    const playerForm = document.createElement("form");
+    const playerForm = document.createElement('form');
 
     //name label and input
-    const nameLabel = document.createElement("label");
-    nameLabel.htmlFor = "name";
-    nameLabel.textContent = "Name: ";
+    const nameLabel =  document.createElement('label');
+    nameLabel.htmlFor = 'name';
+    nameLabel.textContent = 'Name: ';
 
-    const nameInput = document.createElement("input");
-    nameInput.type = "text";
-    nameInput.id = "name";
-    nameInput.name = "name";
-    nameInput.placeholder = "Enter name...";
+    const nameInput = document.createElement('input');
+    nameInput.type = 'text';
+    nameInput.id = 'name';
+    nameInput.name = 'name';
+    nameInput.placeholder = 'Enter name...';
 
     // breed label and input
-    const breedLabel = document.createElement("label");
-    breedLabel.htmlFor = "breed";
-    breedLabel.textContent = "Breed: ";
+    const breedLabel = document.createElement('label');
+    breedLabel.htmlFor = 'breed';
+    breedLabel.textContent = 'Breed: ';
 
-    const breedInput = document.createElement("input");
-    breedInput.type = "text";
-    breedInput.id = "breed";
-    breedInput.name = "breed";
-    breedInput.placeholder = "Enter breed...";
+    const breedInput = document.createElement('input');
+    breedInput.type = 'text';
+    breedInput.id = 'breed';
+    breedInput.name = 'breed';
+    breedInput.placeholder = 'Enter breed...';
 
     //status label and input
-    const statusLabel = document.createElement("label");
-    statusLabel.htmlFor = "status";
-    statusLabel.textContent = "Status: ";
+    const statusLabel = document.createElement('label');
+    statusLabel.htmlFor = 'status';
+    statusLabel.textContent = 'Status: ';
 
-    const statusInput = document.createElement("input");
-    statusInput.type = "text";
-    statusInput.id = "status";
-    statusInput.name = "status";
-    statusInput.placeholder = "Enter status...";
+    const statusInput = document.createElement('input');
+    statusInput.type = 'text';
+    statusInput.id = 'status';
+    statusInput.name = 'status';
+    statusInput.placeholder = 'Enter status...';
 
     //image label and input
-    const imageLabel = document.createElement("label");
-    imageLabel.htmlFor = "image_url";
+    const imageLabel = document.createElement('label');
+    imageLabel.htmlFor = 'image_url';
     imageLabel.textContent = "Image URL: ";
 
-    const imageInput = document.createElement("input");
-    imageInput.type = "text";
-    imageInput.id = "image_url";
-    imageInput.name = "image_url";
+    const imageInput = document.createElement('input')
+    imageInput.type = 'text';
+    imageInput.id = 'image_url';
+    imageInput.name = 'image_url';
     imageInput.placeholder = "Enter image URL...";
 
     //submit button
-    const submitButton = document.createElement("button");
-    submitButton.type = "submit";
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
     submitButton.textContent = "Submit";
 
     playerForm.append(
       nameLabel,
       nameInput,
-      breedLabel,
+      breedLabel, 
       breedInput,
       statusLabel,
       statusInput,
-      imageLabel,
+      imageLabel, 
       imageInput,
       submitButton
     );
 
-    playerForm.addEventListener("submit", async (event) => {
+    playerForm.addEventListener('submit', async(event) => {
       event.preventDefault();
 
       const form = event.target;
@@ -398,13 +397,15 @@ const renderNewPlayerForm = () => {
       await addNewPlayer(playerData);
 
       form.reset();
-    });
+    })
 
     newPlayerFormEl.appendChild(playerForm);
+  
   } catch (err) {
     console.error("Uh oh, trouble rendering the new player form!", err);
   }
 };
+
 
 //---------------------------------------INITIALIZE FUNCTION---------------------------------------
 /**
